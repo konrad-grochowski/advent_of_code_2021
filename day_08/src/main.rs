7ec0c00f0aaec94a46c37a69c6ed4b9e697680d2
konrad-grochowski <konrad7g@gmail.com>
2021-12-27 02:14:04 +0100

    Reevaluate solutions

--- day_08/src/main.rs
+++ day_08/src/main.rs
@@ -2,19 +2,15 @@ use itertools::Itertools;
 use std::collections::HashMap;
 use std::collections::HashSet;
 use std::fs;
+/*
+TODO: correct implementation for actual tasks;
+so far the main function contains proper algorithm for
+figuring out all the segments
 
-fn first_task(numbers: &[i64]) {}
+*/
+fn first_task() {}
 
-fn second_task(numbers: &[i64]) -> i64 {
-    let mean: i64 = numbers.iter().sum::<i64>() / numbers.len() as i64;
-    println!("{}", mean);
-    numbers
-        .iter()
-        .map(|n| {
-            let dist = (n - mean).abs();
-            (dist + 1) * dist / 2
-        })
-        .sum()
+fn second_task() {
 }
 
 fn main() {
@@ -27,7 +23,6 @@ fn main() {
         .iter()
         .map(|x| x.chars().collect::<HashSet<char>>())
         .collect();
-
 
     let mut digit_map: HashMap<u8, HashSet<char>> = HashMap::new();
     let easy_recognition_digits: [(u8, usize); 4] = [(1, 2), (7, 3), (4, 4), (8, 7)];
@@ -50,7 +45,7 @@ fn main() {
         .iter()
         .find(|set| digit_map.get(&7).unwrap().is_subset(set))
         .unwrap();
-    digit_map.insert(3,three_hashset.to_owned());
+    digit_map.insert(3, three_hashset.to_owned());
     let two_hashset = sets_with_five_chars
         .iter()
         .find(|set| {
@@ -64,7 +59,7 @@ fn main() {
                 == 2 as usize
         })
         .unwrap();
-    digit_map.insert(2,two_hashset.to_owned());
+    digit_map.insert(2, two_hashset.to_owned());
     let five_hashset = sets_with_five_chars
         .iter()
         .find(|set| {
@@ -78,7 +73,7 @@ fn main() {
                 == 3 as usize
         })
         .unwrap();
-    digit_map.insert(5,five_hashset.to_owned());
+    digit_map.insert(5, five_hashset.to_owned());
     let sets_with_six_chars: Vec<HashSet<char>> = digits
         .iter()
         .cloned()
@@ -88,15 +83,15 @@ fn main() {
         .iter()
         .find(|set| digit_map.get(&4).unwrap().is_subset(set))
         .unwrap();
-    digit_map.insert(9,nine_hashset.to_owned());
+    digit_map.insert(9, nine_hashset.to_owned());
     let six_hashset = sets_with_six_chars
         .iter()
         .find(|set| digit_map.get(&5).unwrap().is_subset(set))
         .unwrap();
-    digit_map.insert(6,six_hashset.to_owned());
+    digit_map.insert(6, six_hashset.to_owned());
     let zero_hashset = sets_with_six_chars
         .iter()
         .find(|&set| set != six_hashset && set != nine_hashset)
         .unwrap();
-    digit_map.insert(0,zero_hashset.to_owned());
+    digit_map.insert(0, zero_hashset.to_owned());
 }
